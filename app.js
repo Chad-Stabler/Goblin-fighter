41e0cccccc5579c06103fedd206fea92568bfe8b
Chad Stabler <stablerpsn@gmail.com>
2022-05-04 17:43:08 -0500

    beginning work for form submission and displaying goblins

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // import functions and grab DOM elements
+const form = document.querySelector('form');
 
 // let state
-
+let goblins = [{ name: 'Terry', hp: 1, }, { name: 'Plate Goblin Leader' }];
 // set event listeners 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newData = new FormData(form);
+    let gobName = newData.get('goblin-name');
+    let newGob = { name: gobName, hp: Math.ceil(Math.random() * 5), };
+    newGob.push(goblins);
+    displayGoblins();
+});
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
