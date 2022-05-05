77c604ad02729531687fed1e71f95cf6c65372e3
Chad Stabler <stablerpsn@gmail.com>
2022-05-04 23:23:29 -0500

    finished goblin click handler, still work to be done on bugs

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 import { renderGoblin, } from './utils.js';
 const form = document.querySelector('form');
 const goblinDiv = document.querySelector('.goblins');
+const currentHp = document.getElementById('hp-left');
 
 // let state
+let defeatCount = 0;
 let goblins = [{ name: 'Terry', hp: 1, }, { name: 'Plate Goblin Leader', hp: 5 }];
 let playerHp = 10;
 // set event listeners 
@@ -27,11 +29,36 @@ function displayGoblins() {
     }
 }
 
+function displayPlayerHp() {
+    currentHp.textContent = playerHp;
+}
+
 displayGoblins();
 
 function goblinClickHandler(goblin) {
     if (goblin.hp === 0) return;
     if (playerHp === 0) return;
+
+    const playerHit = Math.random();
+
+    if (playerHit < .5) {
+        goblin.hp--;
+        displayGoblins();
+        alert(`You've done it, you hurt ${goblin.name}`);
+        if (goblin.hp <= 0) {
+            defeatCount++;
+        }
+    } else alert(`${goblin.name} dodged your attack`);
+    
+
+    const gobHit = Math.random();
+
+    if (gobHit < 0.1) {
+        alert(`${goblin.name} has lashed back, striking you with a dirty plate`);
+        playerHp -= 3;
+        displayPlayerHp();
+    }
+
 }
   // get user input
   // use user input to update state 
