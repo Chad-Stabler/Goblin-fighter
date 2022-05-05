96e3d5c4f6a2abaa7941eb7b45faa6fc74187243
Chad Stabler <stablerpsn@gmail.com>
2022-05-05 17:06:42 -0500

    starting logic for perk selection

--- app.js
+++ app.js
@@ -4,12 +4,45 @@ const form = document.querySelector('form');
 const goblinDiv = document.querySelector('.goblins');
 const currentHp = document.getElementById('hp-left');
 const defeatedEl = document.getElementById('defeated');
+const perkSelect = document.getElementById('perks');
 
 // let state
+let playerHitChance = 0.5;
+let goblinHitChance = 0.2;
+let playerDamage = 1;
+let goblinDamage = 2;
 let defeatCount = 0;
 let goblins = [{ name: 'Terry', hp: 1, }, { name: 'Plate Goblin Leader', hp: 5 }];
 let playerHp = 10;
 // set event listeners 
+
+perkSelect.addEventListener('change', () => {
+    switch (perkSelect.value) {
+        case 'none' :
+            playerDamage = 1;
+            goblinDamage = 2;
+            playerHitChance = 0.5;
+            goblinHitChance = 0.2;
+            playerHp = 10;
+            alert('Perks removed');
+            break;
+        case 'double-hit' :
+            playerDamage = 2;
+            playerHitChance = 0.4;
+            goblinHitChance = 0.3;
+            alert('You hit twice as hard, but are less likely to hit and more likely to get hit');
+            break;
+        case 'hp-boost' :
+            playerDamage = 1;
+            playerHitChance = 0.5;
+            goblinHitChance = 0.4;
+            playerHp = 15;
+            alert('You have extra health, but are way more likely to get hit');
+            displayPlayerStats();
+            break;
+    }
+});
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const newData = new FormData(form);
@@ -44,8 +77,8 @@ function goblinClickHandler(goblin) {
 
     const playerHit = Math.random();
 
-    if (playerHit < 0.45) {
-        goblin.hp--;
+    if (playerHit < playerHitChance) {
+        goblin.hp -= playerDamage;
         displayGoblins();
         alert(`You've done it, you hurt ${goblin.name}`);
         if (goblin.hp <= 0) {
@@ -57,9 +90,9 @@ function goblinClickHandler(goblin) {
 
     const gobHit = Math.random();
 
-    if (gobHit < 0.2) {
+    if (gobHit < goblinHitChance) {
         alert(`${goblin.name} has lashed back, striking you with a dirty plate`);
-        playerHp -= 3;
+        playerHp -= goblinDamage;
         displayPlayerStats();
         if (playerHp <= 0) {
             alert('You\'ve been slain, another victim of the plate goblin revolution');
